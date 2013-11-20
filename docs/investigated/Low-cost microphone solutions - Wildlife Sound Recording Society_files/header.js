cc16f6db88c51f895786ba99a9363dcaa9d1d256
Piesi <f.piesik@gmail.com>
2013-11-20 13:53:54 +0100

    cleaned up

--- docs/investigated/Low-cost microphone solutions - Wildlife Sound Recording Society_files/header.js
+++
@@ -1,5 +0,0 @@
-today=new Date();
-month=today.getMonth()+13;
-document.write('<style type="text/css">');
-document.write("body #logo { background-image: url(" + mypfx + "images/hdr" + month + ".jpg); background-color: #d6def1}");
-document.write("</style>");
